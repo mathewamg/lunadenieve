a8444f343364764bb90c9edd09f7377ad532e88a
alexander <practicas@atlanticsystems.es>
2017-03-10 18:57:19 +0000

    integración angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AngularFireModule, AuthProviders, AuthMethods } from "angularfire2";
+import { DbApiService } from './../shared/db-api.service';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -5,6 +7,19 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlFG0TvvPKYTZVRuF6qsDvRiHZ6IUNcLY",
+  authDomain: "lunadenieve-adbde.firebaseapp.com",
+  databaseURL: "https://lunadenieve-adbde.firebaseio.com",
+  storageBucket: "lunadenieve-adbde.appspot.com",
+  messagingSenderId: "201783602907"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+};
 
 @NgModule({
   declarations: [
@@ -15,7 +30,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -25,6 +41,6 @@ import { TabsPage } from '../pages/tabs/tabs';
     HomePage,
     TabsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, DbApiService]
 })
-export class AppModule {}
+export class AppModule { }
