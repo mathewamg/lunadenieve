b35b624ab03029709c5adb6b3de342ab87af8bab
Mathew <matt.munoz.garcia@gmail.com>
2017-03-10 19:58:54 +0000

    tabs y menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { MenuPage } from "../pages/menu/menu";
+import { SearchPage } from "../pages/search/search";
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    SearchPage,
+    MenuPage,
     HomePage,
     TabsPage
   ],
@@ -20,8 +20,8 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    SearchPage,
+    MenuPage,
     HomePage,
     TabsPage
   ],
